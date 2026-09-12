98f7e81ece290c7174455d3bcb07891f6e816f53
danielorrala555-spce <danielorrala555@gmail.com>
2026-09-12 18:56:49 -0500

    Add cat and food variables

--- cazando.js
+++ cazando.js
@@ -1,8 +1,30 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let anchoGato = 100;
-let altoGato = 60;
+const anchoGato = 100;
+const altoGato = 60;
+const anchoComida = 80;
+const altoComida = 80;
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+function graficarRectangulo(x, y, ancho, alto, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto);
+}
+function iniciarJuego() {
+    comidaX = canvas.width - anchoComida;
+    comidaY= canvas.height - altoComida;
+    gatoX = canvas.width / 2 - anchoGato / 2;
+    gatoY = canvas.height / 2 - altoGato / 2;
+    graficarGato();
+    graficarComida();
+}
 function graficarGato(){
-    ctx.fillStyle = 'black';
-    ctx.fillRect(300, 400, anchoGato, altoGato);
+    graficarRectangulo(gatoX, gatoY, anchoGato, altoGato, '#8a64db');
+}
+
+function graficarComida(){
+    graficarRectangulo(comidaX, comidaY, anchoComida, altoComida, '#d14040');
 }
